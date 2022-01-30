a0ac80ca45f101cec9e227955a8bd54cac958818
Michael Bashford <mikebashford@gmail.com>
2022-01-30 13:38:43 -0600

    Add department function created

--- server.js
+++ server.js
@@ -52,14 +52,17 @@ const presentOptions = () =>
     if(answer.options == 'Add a department')
     {
       //Function to create new department
+      addDepartment();
     }
     if(answer.options == 'Add a role')
     {
       //Function to enter name, salary, and department for created role
+      addRole();
     }
     if(answer.options == 'Add a employee')
     {
       //Function to add first name, last name, role, manager, then add to database
+      addEmployee();
     }
     if(answer.options == 'Update an employee role')
     {
@@ -103,64 +106,49 @@ const getEmployees = () =>
 
 const addDepartment = () =>
 {
-  db.query('SELECT * FROM department', (err, result) =>
-  {
-    if (err) {
-      console.log(err);
-    }
-    console.table(result);
-
-    return inquirer.prompt([
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'department',
+      message: "What is the name of the department you'd like to add?",
+      validate: departmentInput =>
       {
-        type: 'input',
-        name: 'department',
-        message: "What is the name of the department you'd like to add?",
-        validate: departmentInput =>
+        if(departmentInput)
         {
-          if(departmentInput)
-          {
-            return true;
-          }
-          else {
-            console.log('Please enter a department name!');
-            return false;
-          }        
+          return true;
         }
+        else {
+          console.log('Please enter a department name!');
+          return false;
+        }        
       }
-    ])
+    }
+  ])
+  .then((answer) =>
+  {
+    db.query(`INSERT INTO DEPARTMENT (name) VALUES (?)`, answer.department, (err, result) =>
+    {
+      if (err) {
+        console.log(err);
+      }
+      console.table(result);
+      getDepartments();
+    });
   });
 }
 
 const addRole = () =>
 {
-  db.query('SELECT * FROM role', (err, result) =>
-  {
-    if (err) {
-      console.log(err);
-    }
-    return console.table(result);
-  });
+
 }
 
 const addEmployee = () =>
 {
-  db.query('SELECT * FROM employee', (err, result) =>
-  {
-    if (err) {
-      console.log(err);
-    }
-    return console.table(result);
-  });
+
 }
 
 const updateEmployee = () =>
 {
-  db.query('SELECT * FROM employee', (err, result) =>
-  {
-    if (err) {
-      console.log(err);
-    }
-    return console.table(result);
-  });
+
 }
 presentOptions();
